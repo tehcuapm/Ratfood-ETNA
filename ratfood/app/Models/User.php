760159ee0c071208acaffa655bf4395875cfa1fb
garo_n <garo_n@etna-alternance.net>
2022-02-15 14:32:50 +0100

    [DONE] MongoDB extensions & api support

--- ratfood/app/Models/User.php
+++ ratfood/app/Models/User.php
@@ -30,15 +30,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }

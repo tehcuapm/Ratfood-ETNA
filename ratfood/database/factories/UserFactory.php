0ce4e81fbdd2f9fa8574180826ce254c6054301a
garo_n <garo_n@etna-alternance.net>
2022-02-16 16:48:28 +0100

    [ADD] factories & seeder for Users

--- ratfood/database/factories/UserFactory.php
+++ ratfood/database/factories/UserFactory.php
@@ -23,7 +23,7 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'age' =>$this->faker->randomDigitNotZero(),
-            'password' => $this->faker->password(8,30), // password
+            'password' => "Ouho", // password
 
         ];
     }
--- ratfood/database/seeders/DatabaseSeeder.php
+++ ratfood/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Schema;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        $user = User::factory(10)->create();
     }
 }

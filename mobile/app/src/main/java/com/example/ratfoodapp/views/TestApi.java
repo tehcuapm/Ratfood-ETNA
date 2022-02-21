e800ad1b41cb1fdfa5406385f013368a508a8ee6
garo_n <garo_n@etna-alternance.net>
2022-02-21 14:45:52 +0100

    Changement de branche

---
+++ mobile/app/src/main/java/com/example/ratfoodapp/views/TestApi.java
@@ -0,0 +1,16 @@
+package com.example.ratfoodapp.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ratfoodapp.R;
+
+public class TestApi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_api);
+    }
+}

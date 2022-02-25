434320c2c85856aa03fc67579894d358cb636a5d
pauche_m <pauche_m@etna-alternance.net>
2022-02-25 11:14:34 +0100

    start front

---
+++ mobile/app/src/main/java/com/example/ratfoodapp/views/RestaurantsActivity.java
@@ -0,0 +1,28 @@
+package com.example.ratfoodapp.views;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+
+import com.example.ratfoodapp.R;
+
+public class RestaurantsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.restaurants_layout);
+
+    }
+
+}

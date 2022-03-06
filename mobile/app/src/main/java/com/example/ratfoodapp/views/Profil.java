9d8cc59b1a6a57936b33a3e1f7b04effa7eda215
garo_n <garo_n@etna-alternance.net>
2022-03-06 16:44:22 +0100

    Logo edited

--- mobile/app/src/main/java/com/example/ratfoodapp/views/Profil.java
+++ mobile/app/src/main/java/com/example/ratfoodapp/views/Profil.java
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -18,28 +19,30 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.ratfoodapp.databinding.ProfilLayoutBinding;
 
+import java.util.HashMap;
+
 public class Profil extends AppCompatActivity {
+    private HashMap<String,String> user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profil_layout);
+        this.user = LoginActivity.getInstance().giveAllUser();
+
+
+        TextView username = findViewById(R.id.username_text);
+        username.setText(this.user.get("username"));
+        TextView name = findViewById(R.id.name_text);
+        name.setText(this.user.get("name"));
+        TextView firstname = findViewById(R.id.fname_text);
+        firstname.setText(this.user.get("firstname"));
 
         Button btn_rest = findViewById(R.id.btn_restaurant);
-        btn_rest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(Profil.this, RestaurantsActivity.class));
-            }
-        });
+        btn_rest.setOnClickListener(view -> startActivity(new Intent(Profil.this, RestaurantsActivity.class)));
 
         Button btn_prof = findViewById(R.id.btn_profil);
-        btn_prof.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(Profil.this, LoginActivity.class));
-            }
-        });
+        btn_prof.setOnClickListener(view -> startActivity(new Intent(Profil.this, LoginActivity.class)));
     }
 
 }
